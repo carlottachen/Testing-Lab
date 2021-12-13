f679ec7e647514be952eb4614d68049dee1a282f
Carlotta Chen <carlottachen92@gmail.com>
2021-12-13 14:55:13 -0800

    Completed testing lab

---
+++ functions.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	returnTwo: () => {
+		return 2;
+	},
+
+	greeting: (name) => {
+		return `Hello, ${name}.`;
+	},
+
+	add: (num1, num2) => {
+		return num1 + num2;
+	},
+
+	multiply: (num1, num2) => {
+		return num1 * num2;
+	}
+}
+
---
+++ functions.test.js
@@ -0,0 +1,35 @@
+const {
+	returnTwo,
+	greeting,
+	add,
+	multiply
+} = require('./functions.js');
+
+test('Returns the number 2', () => expect(returnTwo()).toBe(2));
+test('Name displays in greeting', () => {
+	let name = greeting('Carlotta');
+	expect(name).toBe('Hello, Carlotta.');
+});
+
+describe('Math Functions tests', () => {
+	
+	test('Multiplying correctly', () => {
+		let product = multiply(2, 4);
+		expect(product).toBe(8);
+	});
+
+	test('Adding correctly', () => {
+		let sum = add(2, 3);
+		expect(sum).toBe(5);
+	});
+
+	test('Data type of Add', () => {
+		let sum = add(1, 1);
+		expect(typeof sum).toBe('number');
+	});
+
+	test('Alpha character entered in multiply func', () => {
+		let product = multiply('asdf', 2);
+		expect(product).toBe(NaN);
+	});
+});
